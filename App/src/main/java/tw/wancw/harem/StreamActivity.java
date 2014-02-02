dd9c3cc04156ac1e076b9d289e92debcdb1d14cf
WanCW <wancw.wang@gmail.com>
2014-02-02 08:05:20 +0800

    Remove options menu.

--- App/src/main/java/tw/wancw/harem/StreamActivity.java
+++ App/src/main/java/tw/wancw/harem/StreamActivity.java
@@ -2,8 +2,6 @@ package tw.wancw.harem;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class StreamActivity extends ActionBarActivity {
 
@@ -17,27 +15,6 @@ public class StreamActivity extends ActionBarActivity {
                     .add(R.id.container, new StreamFragment())
                     .commit();
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.stream, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
