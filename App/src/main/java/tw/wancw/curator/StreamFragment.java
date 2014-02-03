ccf5ad506c554f4afbb263501638b68bb18b59f0
WanCW <wancw.wang@gmail.com>
2014-02-03 11:32:38 +0800

    Infinite scrolling.

--- App/src/main/java/tw/wancw/curator/StreamFragment.java
+++ App/src/main/java/tw/wancw/curator/StreamFragment.java
@@ -1,23 +1,18 @@
 package tw.wancw.curator;
 
 import android.app.Activity;
-import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ListView;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.PauseOnScrollListener;
-
-import org.apache.http.Header;
 
 import java.util.Collection;
 
@@ -25,13 +20,19 @@ import tw.wancw.curator.api.CuratorApi;
 import tw.wancw.curator.api.MeiZiCard;
 import tw.wancw.curator.api.MeiZiCardsResponseHandler;
 import tw.wancw.curator.widget.MeiZiCardAdapter;
+import tw.wancw.widget.ListViewOnScrollListenerBroadcaster;
 
 public class StreamFragment extends Fragment {
 
     private static final CuratorApi api = new CuratorApi(BuildConfig.CURATOR_API_TOKEN);
 
-    private MeiZiCardAdapter adapter;
     private ImageLoader loader;
+
+    protected MeiZiCardAdapter adapter;
+    protected ListView cardsView;
+    protected View loadingFooter;
+
+    protected PaginatedStreamLoader streamLoader = new PaginatedStreamLoader();
 
     public StreamFragment() {
         // Required empty public constructor
@@ -53,15 +54,6 @@ public class StreamFragment extends Fragment {
         loader.init(imageLoaderConfig);
 
         adapter = new MeiZiCardAdapter(activity, loader);
-        adapter.registerDataSetObserver(new DataSetObserver() {
-            @Override
-            public void onChanged() {
-                update();
-            }
-        });
-    }
-
-    private void update() {
     }
 
     @Override
@@ -69,9 +61,19 @@ public class StreamFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_stream, container, false);
 
-        ListView cardsView = (ListView) rootView.findViewById(R.id.cards);
+        loadingFooter = inflater.inflate(R.layout.view_load_more, null);
+
+        cardsView = (ListView) rootView.findViewById(R.id.cards);
+        cardsView.setEmptyView(rootView.findViewById(android.R.id.empty));
+
+        cardsView.addFooterView(loadingFooter);
         cardsView.setAdapter(adapter);
-        cardsView.setOnScrollListener(new PauseOnScrollListener(loader, true, true));
+        cardsView.removeFooterView(loadingFooter);
+
+        cardsView.setOnScrollListener(new ListViewOnScrollListenerBroadcaster(
+            new PauseOnScrollListener(loader, true, true),
+            new LoadMoreTrigger()
+        ));
 
         return rootView;
     }
@@ -79,13 +81,56 @@ public class StreamFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+        streamLoader.loadNextPage();
+    }
 
-        api.stream(new MeiZiCardsResponseHandler() {
-            @Override
-            public void onSuccess(Collection<MeiZiCard> cards) {
-                Log.d("Curator/Stream", "cards loaded.");
-                adapter.add(cards);
+    private class LoadMoreTrigger implements AbsListView.OnScrollListener {
+
+        private boolean reachEnd = false;
+
+        @Override
+        public void onScroll(AbsListView absListView, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+            if (visibleItemCount > 0 && firstVisibleItem + visibleItemCount >= totalItemCount) {
+                streamLoader.loadNextPage();
             }
-        });
+        }
+
+        @Override
+        public void onScrollStateChanged(AbsListView absListView, int scrollState) {
+            // Do nothing
+        }
+    }
+
+    private class PaginatedStreamLoader implements MeiZiCardsResponseHandler {
+
+        private int lastPage = 0;
+        private boolean loading = false;
+        private boolean noMoreData = false;
+
+        public void loadNextPage() {
+            if (loading || noMoreData) {
+                return;
+            }
+
+            loading = true;
+
+            cardsView.addFooterView(loadingFooter);
+
+            lastPage = lastPage + 1;
+            api.stream(lastPage, this);
+        }
+
+        @Override
+        public void onSuccess(Collection<MeiZiCard> cards) {
+            if (cards.size() > 0) {
+                adapter.add(cards);
+            } else {
+                noMoreData = true;
+            }
+
+            cardsView.removeFooterView(loadingFooter);
+
+            loading = false;
+        }
     }
 }
