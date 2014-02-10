9dc20cadcfa5d895a9ae9d2e4f97622ef4236d2d
WanCW <wancw.wang@gmail.com>
2014-02-10 15:34:48 +0800

    Add MeiZi card detail activity.

---
+++ App/src/main/java/tw/wancw/curator/MeiZiCardDetailActivity.java
@@ -0,0 +1,54 @@
+package tw.wancw.curator;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.cache.disc.impl.FileCountLimitedDiscCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.utils.StorageUtils;
+
+import java.io.File;
+
+public class MeiZiCardDetailActivity extends Activity {
+
+    public static final String PARAM_TITLE = "title";
+    public static final String PARAM_IMAGE_URL = "image_url";
+
+    private ImageLoader imageLoader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_meizi_card_detail);
+
+        DisplayImageOptions displayImageOptions = new DisplayImageOptions.Builder()
+            .cacheInMemory(true)
+            .cacheOnDisc(true)
+            .build();
+
+        File cacheDir = StorageUtils.getCacheDirectory(this);
+        ImageLoaderConfiguration imageLoaderConfig = new ImageLoaderConfiguration.Builder(this)
+            .defaultDisplayImageOptions(displayImageOptions)
+            .discCache(new FileCountLimitedDiscCache(cacheDir, 250))
+            .build();
+
+        imageLoader = ImageLoader.getInstance();
+        imageLoader.init(imageLoaderConfig);
+
+        String title= getIntent().getExtras().getString(PARAM_TITLE);
+        setTitle(title);
+
+        String imageUrl = getIntent().getExtras().getString(PARAM_IMAGE_URL);
+        ImageView imageView = (ImageView) findViewById(R.id.card_image);
+        imageLoader.displayImage(imageUrl, imageView);
+    }
+
+}
